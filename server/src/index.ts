76a5b23dad5d7addf93edc0f431d3dbd10592a6b
unknown <tdossman.development@gmail.com>
2024-05-15 12:55:34 -0400

    connection to MongoDB established

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,29 @@
-import express, { Request, Response } from "express"; 
-import cors from "cors"; 
+// import necessary dependencies
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 3001; 
-const app = express(); 
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+
+// set up the express app
+const PORT = process.env.PORT || 7000;
+const app = express();
+
+// add middleware to parse JSON and urlencoded request bodies
 app.use(express.json())
 app.use(cors())
 
+// define a route handler for the default home page
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello!" });
 });
 
+// start the Express server
 app.listen(PORT, () => {
-  console.log(`API server running on port http://localhost:${PORT}`);
+  console.log(`API server running on port http://localhost:${PORT}/test`);
 });
