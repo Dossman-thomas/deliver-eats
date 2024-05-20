0d61ce9b190532fef8c4b1f321cfa53b4b79d1ce
unknown <tdossman.development@gmail.com>
2024-05-20 12:16:47 -0400

    Header component initialized w/simple text link and bottom border

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './layouts/layout';
 
 const AppRoutes = () => {
 
     return (
-        <Routes>
-            <Route path="/" element={<span>HOME PAGE</span>} />
+        <Routes> 
+            <Route path="/" element={<Layout>HOME PAGE</Layout>} />
             <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
