ca914c20be4e8a2c48b511d27f5444d8403bfac5
unknown <tdossman.development@gmail.com>
2024-05-21 10:22:24 -0400

    Mobile Nav configured w/simple navigation.  Needs updating with more links later, as I build

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
@@ -12,6 +13,10 @@ const Header = () => {
                     className="text-3xl font-bold tracking-tight text-orange-500">
                       DeliverEats.com
                 </Link>
+
+                <div className="md:hidden">
+                    <MobileNav />
+                </div>
 
             </div>
 
