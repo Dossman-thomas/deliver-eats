e3d73e0a50628c08419ed2c9557bdf9675449041
unknown <tdossman.development@gmail.com>
2024-05-23 13:53:46 -0400

    react-query installed and a new instance created in the client/src/main.tsx file for app use.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRoutes'
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
+import React from 'react' // Import the React library
+import ReactDOM from 'react-dom/client' // Import ReactDOM for rendering the application
+import './global.css' // Import global CSS styles
+import { BrowserRouter as Router } from 'react-router-dom' // Import the BrowserRouter for handling routing
+import AppRoutes from './AppRoutes' // Import the component that defines the application's routes
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate' // Import the Auth0 provider for authentication
+import { QueryClient, QueryClientProvider } from 'react-query' // Import React Query's client and provider
 
+// Create a new QueryClient instance with default options
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // Disable refetching on window focus for all queries
+    }
+  }
+})
+
+// Render the React application to the root DOM node
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+  <React.StrictMode> 
+    {/* StrictMode helps to detect potential problems in an application */}
+    <Router> 
+      {/* Router component for enabling routing */}
+      <QueryClientProvider client={queryClient}> 
+        {/* Provide the QueryClient to the React Query context */}
+        <Auth0ProviderWithNavigate> 
+          {/* Provide authentication context using Auth0 */}
+          <AppRoutes /> 
+          {/* Render the application's routes */}
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 )
