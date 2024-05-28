336da6728dcfee3025b236cdf53c743c4d961281
unknown <tdossman.development@gmail.com>
2024-05-28 12:48:34 -0400

    successfully linked local DB w/auth0 so newly created users are saved in our local database.  Also added the 'concurrently' script to root package.json to start frontend and backend servers together with one script command 'npm run start'

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,5 @@
 // Import necessary modules and types from the @auth0/auth0-react package and the events module
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 // import { on } from "events";
 
@@ -9,7 +10,9 @@ type Props = {
 
 // Define the Auth0ProviderWithNavigate component, which takes children as props
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-    
+  
+  const { createUser } = useCreateMyUser(); // Call the useCreateMyUser hook to create a new user
+  
   const domain = import.meta.env.VITE_AUTH0_DOMAIN; // Retrieve Auth0 domain from environment variables
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID; // Retrieve Auth0 client ID from environment variables
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL; // Retrieve Auth0 redirect URI from environment variables
@@ -22,11 +25,17 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
 
-// Define a callback function to handle redirects, optionally accepting appState and user parameters
+// Define a callback function to handle redirects from the auth0 login and signup forms, optionally accepting appState and user parameters
   const onRedirectCallback = (appState?: AppState, user?: User) => {
     // Log the user object to the console
     console.log("USER", user);
-  }
+
+    // Check if the user object exists and contains the sub and email properties
+    if(user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email }); // Call the createUser function with the user's Auth0 ID and email
+    }
+
+  };
 
   return ( // Return the Auth0Provider component with the domain, client ID, redirect URI, and onRedirectCallback function as props and required children
     <Auth0Provider 
