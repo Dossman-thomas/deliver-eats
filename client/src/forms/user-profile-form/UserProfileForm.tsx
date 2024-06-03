cf2be22dff2b4ad6e7d39fc0d51e812c390f421f
unknown <tdossman.development@gmail.com>
2024-06-03 10:42:39 -0400

    User profile form created along with conditional logic to render a 'loading' button when submitting the form

--- client/src/forms/user-profile-form/UserProfileForm.tsx
+++ client/src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,16 +1,173 @@
-import { z } from 'zod';  // Importing the zod library
+import { z } from "zod"; // Importing the zod library
+import { useForm } from "react-hook-form"; // Importing the react-hook-form library
+import { zodResolver } from "@hookform/resolvers/zod"; // Importing the zod resolver from the hook form library
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
 
 // Defining the schema for form validation using zod
 const formSchema = z.object({
-    email: z.string().optional(), // Email is optional and must be a string if provided
-    name: z.string().min(1, "Name is required"), // Name is required and must be a string with at least 1 character
-    addressLine1: z.string().min(1, "Address Line 1 is required"), // Address Line 1 is required and must be a string with at least 1 character
-    addressLine2: z.string().optional(), // Address Line 2 is optional and must be a string if provided
-    city: z.string().min(1, "City is required"), // City is required and must be a string with at least 1 character
-    state: z.string().min(1, "State is required"), // State is required and must be a string with at least 1 character
-    country: z.string().min(1, "Country is required"), // Country is required and must be a string with at least 1 character
-    zipCode: z.string().min(1, "Zip Code is required"), // Zip Code is required and must be a string with at least 1 character
+  email: z.string().optional(), // Email is optional and must be a string if provided
+  name: z.string().min(1, "Name is required"), // Name is required and must be a string with at least 1 character
+  addressLine1: z.string().min(1, "Address Line 1 is required"), // Address Line 1 is required and must be a string with at least 1 character
+  addressLine2: z.string().optional(), // Address Line 2 is optional and must be a string if provided
+  city: z.string().min(1, "City is required"), // City is required and must be a string with at least 1 character
+  state: z.string().min(1, "State is required"), // State is required and must be a string with at least 1 character
+  country: z.string().min(1, "Country is required"), // Country is required and must be a string with at least 1 character
+  zipCode: z.string().min(1, "Zip Code is required"), // Zip Code is required and must be a string with at least 1 character
 });
 
 // Inferring the TypeScript type from the form schema
-type UserFormData = z.infer<typeof formSchema>; 
+type UserFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+};
+
+const UserProfileForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<UserFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col md:flex-row gap-4">
+          <FormField
+            control={form.control}
+            name="addressLine1"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="addressLine2"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address Line 2</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="state"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>State</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Country</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="zipCode"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Zip Code</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        {isLoading ? (
+          <LoadingButton />
+        ) : (
+          <Button type="submit" className="bg-orange-500 ">
+            Submit
+          </Button>
+        )}
+        
+      </form>
+    </Form>
+  );
+};
+
+export default UserProfileForm;
