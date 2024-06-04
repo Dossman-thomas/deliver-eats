52b68a7aa0340e0f8f600670fffbcb9ffbe5a020
unknown <tdossman.development@gmail.com>
2024-06-04 11:09:16 -0400

    backend api route to get current user created

--- server/src/controllers/MyUserController.ts
+++ server/src/controllers/MyUserController.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from 'express'; // Import Request and Response types from express
 import User from '../models/user'; // Import the User model
 
+// Function to get current user
+const getCurrentUser = async (req: Request, res: Resp) => {
+    try {
+
+        const currentUser = await User.findOne({ _id: req.userId });
+
+        if(!currentUser){
+
+            return res.status(404).json({ message: "User not found" }); 
+
+        }
+
+        res.json(currentUser); 
+        
+    } catch (error) {
+
+        console.log(error); 
+        res.status(500).json({ message: "Something went wrong" });
+
+    }
+}
+
+
 // Function to create a new user or return an existing user based on the provided auth0Id
 const createCurrentUser = async (req: Request, res: Response) => {
     try {
@@ -68,6 +91,7 @@ const updateCurrentUser = async (req: Request, res: Response) => {
 }
 
 export default {
+    getCurrentUser, // Export the getCurrentUser function
     createCurrentUser, // Export the createCurrentUser function
     updateCurrentUser, // Export the updateCurrentUser function
 };
--- server/src/routes/MyUserRoute.ts
+++ server/src/routes/MyUserRoute.ts
@@ -6,19 +6,30 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post(
+// /api/my/user
+
+router.get(
+    "/",
+    jwtCheck, 
+    jwtParse, 
+    MyUserController.getCurrentUser); 
+)
+
+router.post( // API endpoint to create a new user from the auth0 data
     "/", 
     jwtCheck, 
     MyUserController.createCurrentUser
 );
 
-router.put(
+router.put( // API endpoint to update a user 
     "/", 
     jwtCheck, 
     jwtParse, 
     validateMyUserRequest, 
     MyUserController.updateCurrentUser
 );
+
+
 
 export default router;
 
