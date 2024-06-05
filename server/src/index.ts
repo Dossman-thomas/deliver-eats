60ac31c9dffbbf625a28a81c6338c1a2bb2640ce
unknown <tdossman.development@gmail.com>
2024-06-05 11:29:33 -0400

    health endpoint added to server index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,21 +6,25 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 
 // connect to deliverEats MongoDB
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to MongoDB");
+});
 
 // set up the express app
 const PORT = process.env.PORT || 7000;
 const app = express();
 
 // add middleware to parse JSON and urlencoded request bodies
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+// add a "health" endpoint to check if the server is running properly. This is useful for monitoring and alerting purposes.
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health okay!" });
+});
 
 // define a route handler for the user page
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
 
 // start the Express server
 app.listen(PORT, () => {
