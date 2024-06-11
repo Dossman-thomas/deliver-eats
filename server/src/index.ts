fb91012f01d6466be0f01abd7b3d6aa2aaeebbe6
unknown <tdossman.development@gmail.com>
2024-06-11 09:04:58 -0400

    preparing files and directories to seperate concerns for purposes of deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,8 +19,8 @@ app.use(express.json());
 app.use(cors());
 
 // Serve static files from the client build directory
-const clientBuildPath = path.join(__dirname, '../../client/dist');
-app.use(express.static(clientBuildPath));
+// const clientBuildPath = path.join(__dirname, '../../client/dist');
+// app.use(express.static(clientBuildPath));
 
 // add a "health" endpoint to check if the server is running properly. This is useful for monitoring and alerting purposes.
 app.get("/health", async (req: Request, res: Response) => {
@@ -31,11 +31,11 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 
 // For any other route, serve the client’s index.html file
-app.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(clientBuildPath, 'index.html'));
-});
+// app.get('*', (req: Request, res: Response) => {
+//   res.sendFile(path.join(clientBuildPath, 'index.html'));
+// });
 
 // start the Express server with '0.0.0.0' as the second argument to ensure the server binds to all available network interfaces, making it accessible from outside the local machine.
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`API server running on port http://localhost:${PORT}`);
 });
