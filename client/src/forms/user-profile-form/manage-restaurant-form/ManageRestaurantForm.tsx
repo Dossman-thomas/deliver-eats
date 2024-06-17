caf256901f4ba8e305b48c1afa71fa623e707d39
Dossman-thomas <tdossman.development@gmail.com>
2024-06-17 13:10:16 -0400

    Cuisines Checkbox section created and mapped out on page

--- client/src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ client/src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
 
 const formSchema = z.object({
   // creating a schema for form validation
@@ -64,6 +66,8 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         className="space-y-8 bg-gray-50 p-10 rounded-lg"
       >
         <DetailsSection />
+        <Separator />
+        <CuisinesSection />
       </form>
     </Form>
   );
